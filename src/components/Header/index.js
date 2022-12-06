8c6707f6d44b76d7dd0572a3916fa5d895ecc1c2
R-one94 <e.lable@yahoo.fr>
2022-12-06 10:52:29 +0100

    first step header-structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import './style.scss';
+import logo from '../../assets/images/OTROC-logo.png';
 
 function Header() {
   return (
     <header className="header">
-      <h1> O'TROC </h1>
-      <button type="button"> Nav</button>
-      <button type="button"> Se connecter / s'inscrire</button>
+      <img src={logo} alt="Logo OTroc" />
+      <div className="header-nav">
+        <button type="button"> Nav</button>
+        <button type="button"> Se connecter / s'inscrire</button>
+      </div>
     </header>
   );
 }
