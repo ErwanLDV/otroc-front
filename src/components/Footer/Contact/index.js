628402dab42680fe4f3e3dc600a0a57b1eea4738
R-one94 <e.lable@yahoo.fr>
2022-12-06 17:44:20 +0100

    static pages of footer

--- src/components/Footer/Contact/index.js
+++ src/components/Footer/Contact/index.js
@@ -4,6 +4,7 @@ function Contact() {
   return (
     <section>
       <h1> Nous contacter </h1>
+      <p>Envoyez nous un mail a cette adresse<br /> <a href="mailto:o-troc@localhost.fr">o-troc@localhost.fr</a></p>
     </section>
   );
 }
