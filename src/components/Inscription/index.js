394828de7c636a8186e673036a28e069da3129b2
R-one94 <e.lable@yahoo.fr>
2022-12-07 16:06:58 +0100

    inscription-component first version no style

--- src/components/Inscription/index.js
+++ src/components/Inscription/index.js
@@ -3,11 +3,30 @@ import './style.scss';
 function Inscription() {
   return (
     <form className="form-inscription">
-      <p>Créez un compte</p>
-      <input type="email" placeholder="votre adresse email" />
-      <input type="text" placeholder="mot de passe" />
-      <input type="password" placeholder="comfirmer le mot de passe" />
+      <p className="form-inscription-p">Créez un compte</p>
+      <div>
+        <input name="firstname" type="text" placeholder="Prénom" required />
+        <input name="lastname" type="text" placeholder="Nom" required />
+      </div>
+      <input name="username" type="text" placeholder="Pseudo" required />
+      <input name="login" type="email" placeholder="Votre adresse email" required />
+      <input name="password" type="text" placeholder="Mot de passe" required />
+      <input name="zipcode" type="text" placeholder="Code postal" required />
+      <div>
+        <input name="phone-number" type="text" placeholder="Numero de telephone*" required />
+      </div>
+      <div>
+        <label htmlFor="picture">photo de profil*
+          <input
+            name="picture"
+            type="file"
+            accept="image/png, image/jpeg"
+          />
+        </label>
+      </div>
       <button type="submit">Valider</button>
+      <p className="form-inscription-p">*non obligatoire</p>
+
     </form>
   );
 }
