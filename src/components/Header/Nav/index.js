e038796e20e91f13006062a7cb36db5ef5ad98e0
RichardObreme <obremerichard@gmail.com>
2022-12-13 09:56:35 +0100

    First step neww css navbar

--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -5,7 +5,7 @@ import './style.scss';
 function Nav({ categoriesArray }) {
   return (
     <div>
-      <nav role="navigation">
+      {/* <nav role="navigation">
         <ul>
           <li>
             <NavLink
@@ -41,6 +41,29 @@ function Nav({ categoriesArray }) {
             ))
           }
         </ul>
+      </nav> */}
+      <nav className="menu">
+        <ol>
+          <li className="menu-item"><NavLink to="/">Home</NavLink></li>
+          <li className="menu-item"><a href="#0">About</a></li>
+          <li className="menu-item">
+            <a href="#0">test</a>
+            <ol className="sub-menu">
+              <li className="menu-item"><NavLink to="/test">test</NavLink></li>
+              <li className="menu-item"><a href="#0">Bigger Widgets</a></li>
+              <li className="menu-item"><a href="#0">Huge Widgets</a></li>
+            </ol>
+          </li>
+          <li className="menu-item">
+            <a href="#0">Kabobs</a>
+            <ol className="sub-menu">
+              <li className="menu-item"><a href="#0">Shishkabobs</a></li>
+              <li className="menu-item"><a href="#0">BBQ kabobs</a></li>
+              <li className="menu-item"><a href="#0">Summer kabobs</a></li>
+            </ol>
+          </li>
+          <li className="menu-item"><a href="#0">Contact</a></li>
+        </ol>
       </nav>
     </div>
   );
