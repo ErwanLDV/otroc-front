2f5a40ef5199b8c625ba8e8a5813f23f8d7acbc7
RichardObreme <obremerichard@gmail.com>
2022-12-14 09:39:58 +0100

    NavBar CSS done

--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -1,32 +1,40 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './style.scss';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionNavbarRerender } from '../../../actions/categories';
 
-function Nav({ categoriesArray }) {
+function Nav({ categoriesArray, navBarRerender }) {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(actionNavbarRerender(false));
+    setTimeout(() => {
+      dispatch(actionNavbarRerender(true));
+    }, 100);
+  }, [location]);
   return (
     <div>
-      {/* <nav role="navigation">
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-            >
-              Accueil
-            </NavLink>
+      <nav className="menu">
+        <ol>
+          <li className="menu-item">
+            <NavLink to="/">Home</NavLink>
           </li>
           {
             categoriesArray.map((mainCategory) => (
-              <li className="test" key={`${mainCategory.id}`}>
+              <li className="menu-item" key={`${mainCategory.id}`}>
                 <NavLink
                   to={mainCategory.slug}
                   key={`${mainCategory.id}`}
                 >
                   {mainCategory.name}
                 </NavLink>
-                <ul className="dropdown">
-                  {
-                    mainCategory.categories.map((category) => (
-                      <li key={`${mainCategory.id}-${category.id}`}>
+                <ol className="sub-menu">
+                  { navBarRerender
+                    && mainCategory.categories.map((category) => (
+                      <li className="menu-item" key={`${mainCategory.id}-${category.id}`}>
                         <NavLink
                           to={category.slug}
                           key={`${mainCategory.id}-${category.id}`}
@@ -34,35 +42,11 @@ function Nav({ categoriesArray }) {
                           {category.name}
                         </NavLink>
                       </li>
-                    ))
-                  }
-                </ul>
+                    ))}
+                </ol>
               </li>
             ))
           }
-        </ul>
-      </nav> */}
-      <nav className="menu">
-        <ol>
-          <li className="menu-item"><NavLink to="/">Home</NavLink></li>
-          <li className="menu-item"><a href="#0">About</a></li>
-          <li className="menu-item">
-            <a href="#0">test</a>
-            <ol className="sub-menu">
-              <li className="menu-item"><NavLink to="/test">test</NavLink></li>
-              <li className="menu-item"><a href="#0">Bigger Widgets</a></li>
-              <li className="menu-item"><a href="#0">Huge Widgets</a></li>
-            </ol>
-          </li>
-          <li className="menu-item">
-            <a href="#0">Kabobs</a>
-            <ol className="sub-menu">
-              <li className="menu-item"><a href="#0">Shishkabobs</a></li>
-              <li className="menu-item"><a href="#0">BBQ kabobs</a></li>
-              <li className="menu-item"><a href="#0">Summer kabobs</a></li>
-            </ol>
-          </li>
-          <li className="menu-item"><a href="#0">Contact</a></li>
         </ol>
       </nav>
     </div>
@@ -71,6 +55,7 @@ function Nav({ categoriesArray }) {
 
 Nav.propTypes = {
   categoriesArray: PropTypes.array.isRequired,
+  navBarRerender: PropTypes.bool.isRequired,
 };
 
 export default Nav;
