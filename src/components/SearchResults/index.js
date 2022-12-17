d2c6213cd2eddd62c7dc292e2f0ecbd057dc144d
RichardObreme <obremerichard@gmail.com>
2022-12-17 17:04:51 +0100

    fix bug 404 MainCategories & Categories results

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,19 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { actionGetCategoryResults } from '../../actions/categories';
 import './style.scss';
 import AnnoucementCard from '../AnnoucementCard';
+import Error from '../Error';
 
 function SearchResult() {
   const dispatch = useDispatch();
-
+  const [is404, setIs404] = useState(false);
   const slug = useParams();
-  console.log(slug.slug);
 
   const categoriesArray = useSelector((state) => state.categories.categoriesArray);
   const categoriesLoaded = useSelector((state) => state.categories.categoriesLoaded);
-  console.log('categoriesArray', categoriesArray);
 
   useEffect(() => {
     if (categoriesLoaded) {
@@ -21,9 +20,13 @@ function SearchResult() {
         (main) => main.categories.find((subcat) => subcat.slug === slug.slug),
       );
       const subCategory = categoriesArrayResult.filter((element) => element !== undefined);
-      const { id } = subCategory[0];
-
-      dispatch(actionGetCategoryResults(id));
+      if (subCategory.length > 0) {
+        const { id } = subCategory[0];
+        dispatch(actionGetCategoryResults(id));
+      }
+      else {
+        setIs404(true);
+      }
     }
   }, [categoriesLoaded, slug]);
 
@@ -32,37 +35,41 @@ function SearchResult() {
   const { offers, wishes } = categoryResults;
 
   return (
-    <section>
-      <h2>Résultats de la recherche</h2>
-      {categoryResults && offers && offers.map((item) => (
-        <Link to={`/annonces/offers/${item.id}`} key={item.id}>
-          <AnnoucementCard
-            key={item.id}
-            title={item.title}
-            content={item.description}
-            pseudo={item.user.alias}
-            createdAt={item.createdAt}
-            category={item.categories[0].name}
-            logo="offer"
-          // image={item.picture}
-          />
-        </Link>
-      ))}
-      {wishes && wishes.map((item) => (
-        <Link to={`/annonces/wishes/${item.id}`} key={item.id}>
-          <AnnoucementCard
-            key={item.id}
-            title={item.title}
-            content={item.description}
-            pseudo={item.user.alias}
-            createdAt={item.createdAt}
-            category={item.categories[0].name}
-            logo="wish"
-          // image={item.picture}
-          />
-        </Link>
-      ))}
-    </section>
+    is404
+      ? <Error />
+      : (
+        <section>
+          <h2>Résultats de la recherche</h2>
+          {categoryResults && offers && offers.map((item) => (
+            <Link to={`/annonces/offers/${item.id}`} key={item.id}>
+              <AnnoucementCard
+                key={item.id}
+                title={item.title}
+                content={item.description}
+                pseudo={item.user.alias}
+                createdAt={item.createdAt}
+                category={item.categories[0].name}
+                logo="offer"
+              // image={item.picture}
+              />
+            </Link>
+          ))}
+          {wishes && wishes.map((item) => (
+            <Link to={`/annonces/wishes/${item.id}`} key={item.id}>
+              <AnnoucementCard
+                key={item.id}
+                title={item.title}
+                content={item.description}
+                pseudo={item.user.alias}
+                createdAt={item.createdAt}
+                category={item.categories[0].name}
+                logo="wish"
+              // image={item.picture}
+              />
+            </Link>
+          ))}
+        </section>
+      )
   );
 }
 
