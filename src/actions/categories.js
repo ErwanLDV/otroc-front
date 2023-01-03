4bb2bc40a906b2a354ae23d9f8b1fc66c82cf600
RichardObreme <obremerichard@gmail.com>
2023-01-03 13:34:01 +0100

    categories actions comments

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -9,12 +9,21 @@ export const SAVE_MAIN_CATEGORIES_RESULT_FOR_CARDS = 'SAVE_MAIN_CATEGORIES_RESUL
 export const GET_TOP_CATEGORIES = 'GET_TOP_CATEGORIES';
 export const SAVE_TOP_CATEGORIES = 'SAVE_TOP_CATEGORIES';
 
+/**
+ * Action to get all categories from API
+ * @returns {object}
+ */
 export function actionGetAllCategories() {
   return {
     type: GET_ALL_CATEGORIES,
   };
 }
 
+/**
+ * Action to save the array of the categories in the state
+ * @param {Array} categoriesArray array of the categories
+ * @returns {object}
+ */
 export function actionSaveAllCategories(categoriesArray) {
   return {
     type: SAVE_ALL_CATEGORIES,
@@ -22,6 +31,11 @@ export function actionSaveAllCategories(categoriesArray) {
   };
 }
 
+/**
+ * Action to set the boolean to know if the categories are loaded
+ * @param {boolean} bool
+ * @returns {object}
+ */
 export function actionCategoriesLoaded(bool) {
   return {
     type: CATEGORIES_LOADED,
@@ -29,6 +43,11 @@ export function actionCategoriesLoaded(bool) {
   };
 }
 
+/**
+ * Action to set the boolean to rerender the navbar
+ * @param {boolean} bool
+ * @returns {object}
+ */
 export function actionNavbarRerender(bool) {
   return {
     type: NAVBAR_RERENDER,
@@ -36,6 +55,11 @@ export function actionNavbarRerender(bool) {
   };
 }
 
+/**
+ * Action to get the annoucements of one category
+ * @param {string} id of the category
+ * @returns {object}
+ */
 export function actionGetCategoryResults(id) {
   return {
     type: GET_CATEGORY_RESULTS,
@@ -43,6 +67,11 @@ export function actionGetCategoryResults(id) {
   };
 }
 
+/**
+ * Action to the categories of a main category
+ * @param {string} id of the main category
+ * @returns {object}
+ */
 export function actionGetMainCategoryResults(id) {
   return {
     type: GET_MAIN_CATEGORY_RESULTS,
@@ -50,6 +79,11 @@ export function actionGetMainCategoryResults(id) {
   };
 }
 
+/**
+ * Action to save the annoucements of one category in the state
+ * @param {Array} categoriesArray array of the annoucements
+ * @returns {object}
+ */
 export function actionSaveCategoryResults(categoriesArray) {
   return {
     type: SAVE_CATEGORY_RESULTS,
@@ -57,6 +91,11 @@ export function actionSaveCategoryResults(categoriesArray) {
   };
 }
 
+/**
+ * Action to save the categories of one main category in the state
+ * @param {Array} categoriesArray array of the categories
+ * @returns {object}
+ */
 export function actionSaveMainCategoriesResultForCards(categoriesArray) {
   return {
     type: SAVE_MAIN_CATEGORIES_RESULT_FOR_CARDS,
@@ -64,12 +103,21 @@ export function actionSaveMainCategoriesResultForCards(categoriesArray) {
   };
 }
 
+/**
+ * Action to get the top categories from API
+ * @returns {object}
+ */
 export function actionGetTopCategories() {
   return {
     type: GET_TOP_CATEGORIES,
   };
 }
 
+/**
+ * Action to save the top categories in the state
+ * @param {Array} topCategoriesArray array of the top categories
+ * @returns {object}
+ */
 export function actionSaveTopCategories(topCategoriesArray) {
   return {
     type: SAVE_TOP_CATEGORIES,
